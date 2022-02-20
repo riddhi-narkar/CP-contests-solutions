db87d31c6141e9433b4372796d2679b2191f2f9e
Riddhi Narkar <62413157+grey-hat2003@users.noreply.github.com>
2022-02-20 14:21:24 +0530

    added files

---
+++ IEEEXtreme 2021/Doctors_Appointment.cpp
@@ -0,0 +1,24 @@
+#include<bits:stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int t;
+    cin >> t;
+ 
+    while(t--)
+    {
+        int n, l, r;
+        cin >> n;
+        vector <pair <int, int> > vec;
+    
+        for(int i = 0; i<n; i++)
+        {
+            cin >> l >> r;
+            vec.push_back(make_pair(l, r));
+        }
+        
+
+    }
+    return 0;
+}
---
+++ IEEEXtreme 2021/ImageResolution.cpp
@@ -0,0 +1,29 @@
+#include<bits:stdc++.h>
+using namespace std;
+int main()
+{   vector <pair<char,int> > v={{'T',6},{'K',15},{'M',11},{'S',12},{'C',5},{'D',15},{'c',8},{'v',5},{'T',8},{'T',9},{'w',7},{'W',17},{'T',18},{'A',6}};
+    string x;
+    cin>>x;
+    cout << x.length();
+    // for(int a=0; a<14; a++)
+    // {if(x[0]==v[a].first && x.length()==v[a].second)
+    //  cout<<x.length;
+    // }
+    return 0;}
+// char c[]={'T','K','M','S','C','D','c','v','T','T','w','W','T','A'};
+    // int i[]={6,15,11,12,5,15,8,5,8,9,7,17,18,6};
+    // string a[14] = {"Team 1","Knapsackers@UNT","MoraSeekers","SurpriseTeam","CuSAT","DongskarPedongi","cofrades","viRUs","TeamName","TeamEPFL1","whatevs","WildCornAncestors","TheCornInTheFields","Aurora"};
+// T6
+// K15
+// M11
+// S12
+// C5
+// D15 
+// c8
+// v5
+// T8 
+// T9
+// w7
+// W17
+// T18
+// A6
